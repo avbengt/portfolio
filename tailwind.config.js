6659382c021959810ef623a4cf07394e5f4dc802
avbengt <avbengt@gmail.com>
2025-01-22 22:35:58 -0500

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './index.html',       // Main HTML file
-    './js/**/*.js',       // All JavaScript files in the js folder and subfolders
-    './css/**/*.css',     // All CSS files in the css folder and subfolders
+    './index.html',
+    './js/**/*.js',
+    './css/**/*.css',
   ],
   theme: {
     extend: {
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
-        heading: ['Lora', 'serif'],
+        fjord: ['Fjord One', 'serif'],
       },
     },
   },
